d28e429ebcab92a50c342ccad856f50daada7b0a
Justin Hall <justin.r.hall@gmail.com>
2024-08-21 20:35:11 -0600

    Render a single image

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
-import { getAllProjects } from './lib/projects';
-import ProjectList from './components/project-list';
+import Image from 'next/image';
+import defaultImage from '../public/static/images/default.jpg';
+import { breakpoints } from './utils/breakpoints';
 
 export default async function IndexRoute() {
-  const projects = await getAllProjects();
-
   return (
     <div className="flex min-h-screen flex-col items-center bg-gray-300 p-4">
-      <ProjectList projects={projects} />
+      <Image
+        className="rounded-full"
+        src={defaultImage}
+        alt="Avatar"
+        sizes={`(min-width: ${breakpoints.lg}) 192px, 128px`}
+        fill
+        priority
+      />
     </div>
   );
 }
