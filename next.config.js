85d679cc86c6013a33894ffb80f2947c6896c4da
Justin Hall <justin.r.hall@gmail.com>
2024-08-23 17:12:40 -0600

    Remove entire image section from Next config

--- next.config.js
+++ next.config.js
@@ -5,10 +5,6 @@ import withPlaiceholder from '@plaiceholder/next';
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  images: {
-    deviceSizes: [500],
-    formats: ['image/avif', 'image/webp'],
-  },
   reactStrictMode: true,
 };
 
