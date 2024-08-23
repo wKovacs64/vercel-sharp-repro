32e33a46d85ac9bc135f6ee0ec64327d22d96685
Justin Hall <justin.r.hall@gmail.com>
2024-08-23 17:24:06 -0600

    Transpile @plaiceholder/next package in Next config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ import withPlaiceholder from '@plaiceholder/next';
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  transpilePackages: ['@plaiceholder/next'],
   reactStrictMode: true,
 };
 
