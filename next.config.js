644b9b94a0d60288303c7c89deec6a867bd5dbc0
Justin Hall <justin.r.hall@gmail.com>
2024-08-23 17:00:57 -0600

    Bring back deviceSizes and remove 331 from imageSizes

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ import withPlaiceholder from '@plaiceholder/next';
  */
 const nextConfig = {
   images: {
-    // deviceSizes: [500],
-    imageSizes: [128, 192, 331, 386, 480],
+    deviceSizes: [500],
+    imageSizes: [128, 192, 386, 480],
     formats: ['image/avif', 'image/webp'],
   },
   reactStrictMode: true,
